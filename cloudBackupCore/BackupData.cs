c33508b663dcb2418592250734ee85d6369a0966
Roger Briggen <roger.briggen@bluewin.ch>
2021-10-11 19:00:10 +0200

    Update to .Net 5

---
+++ cloudBackupCore/BackupData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace cloudBackupCore
+{
+    public class BackupRoot
+    {
+        public string RemoteRootPath { get; set; }
+        public BackupLocalRoot[] localRoots { get; set; }
+       
+    }
+
+
+    public class BackupLocalRoot
+    {
+        public string LocalRootPath { get; set; }
+        public string[] include;
+        public string[] exclude;
+    }
+
+
+    public class BackupJob
+    {
+        public BackupRoot BackupRoot { get; set; }
+        public string[] InBackupIncluded;
+        public string[] InBackupExclude;
+        public DateTime LastSuccessfulRun;
+    }
+}
--- cloudBackupCore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace cloudBackupCore
-{
-    public class Class1
-    {
-    }
-}
